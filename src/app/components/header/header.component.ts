d927c041661df3de1eeba83eb797b2e073ac840b
Usamah Kamil <usamah.kamil@gmail.com>
2021-02-06 04:10:53 +0530

    header on scroll changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { Subject } from 'rxjs';
 import { takeUntil, tap } from 'rxjs/operators';
 import { AppService } from 'src/app/services/app.service';
@@ -9,13 +9,22 @@ import { AppService } from 'src/app/services/app.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  @ViewChild('headerRef', { static: false }) headerRef: any;
   destroyed$: Subject<any> = new Subject();
   menu: Array<any> = [];
   user: any = {};
 
+  @HostListener('window:scroll', ['$event']) onScroll(event: Event) {
+    if (window.scrollY >= 100) {
+      this.renderer.addClass(this.headerRef.nativeElement, 'scrolling');
+    } else {
+      this.renderer.removeClass(this.headerRef.nativeElement, 'scrolling');
+    }
+  }
+
   constructor(
-    private app: AppService
+    private app: AppService,
+    private renderer: Renderer2
   ) { }
 
   ngOnInit(): void {
