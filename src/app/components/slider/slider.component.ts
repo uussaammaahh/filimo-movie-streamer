487c98086272a597dbc0615ed285eff0aaa43be9
Usamah Kamil <usamah.kamil@gmail.com>
2021-02-06 14:20:22 +0530

    common slider component with functionality

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import SwiperCore from 'swiper/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import SwiperCore, { SwiperOptions, Navigation } from 'swiper/core';
 
 @Component({
   selector: 'app-slider',
@@ -11,8 +11,24 @@ export class SliderComponent implements OnInit {
   @Input() sliderData: any;
   @Input() episodes: boolean = false;
 
+  randomClass: any = 'btn-'+btoa(Math.random().toString()).replace(/[^a-zA-Z]/g, '');
+  config: SwiperOptions = {
+    slidesPerView: 5,
+    spaceBetween: 15,
+    navigation: {
+      nextEl: '.button-next.' + this.randomClass,
+      prevEl: '.button-prev.' + this.randomClass,
+    }
+  };
+
   constructor() { }
 
-  ngOnInit() { }
-  
+  ngOnInit() {
+    SwiperCore.use([Navigation]);
+    setTimeout(() => {
+      console.log(this.sliderData);
+      
+    }, 1000);
+  }
+
 }
